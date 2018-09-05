0c601627c5fcdad3ae199106895a4213ab329cd0
Евгений Глухов <Schumacher2008@list.ru>
2018-09-05 23:49:22 +0300

    Initial Commit

---
+++ EGSpinner/EGSpinner.h
@@ -0,0 +1,19 @@
+//
+//  EGSpinner.h
+//  EGSpinner
+//
+//  Created by Евгений Глухов on 05.09.2018.
+//  Copyright © 2018 Евгений Глухов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EGSpinner.
+FOUNDATION_EXPORT double EGSpinnerVersionNumber;
+
+//! Project version string for EGSpinner.
+FOUNDATION_EXPORT const unsigned char EGSpinnerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EGSpinner/PublicHeader.h>
+
+
